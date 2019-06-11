a6ae7dea75f809f6bc1e2cd7148308dca25f9434
raibm <www.rai12@hotmail.com>
2019-06-11 16:24:52 -0300

    alteracoes no repositorio

--- src/main/java/com/airfare/airfareapp/repository/AirfareRepository.java
+++ src/main/java/com/airfare/airfareapp/repository/AirfareRepository.java
@@ -3,5 +3,9 @@ package com.airfare.airfareapp.repository;
 import com.airfare.airfareapp.domain.Airfare;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AirfareRepository extends JpaRepository<Airfare, Integer> {
+    List<Airfare> findById(int id);
+
 }
--- src/main/java/com/airfare/airfareapp/repository/UserRepository.java
+++ src/main/java/com/airfare/airfareapp/repository/UserRepository.java
@@ -3,5 +3,10 @@ package com.airfare.airfareapp.repository;
 import com.airfare.airfareapp.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    List<User> findById(int id);
+
 }
